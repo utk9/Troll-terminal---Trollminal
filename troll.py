2ecb9e72ec7410278385549e40ce919e2d21a48a
utk9 <utkarshlamba.2@gmail.com>
2016-11-26 15:06:56 -0500

    set up cases

--- troll.py
+++ troll.py
@@ -1,15 +1,42 @@
 import os
+import re
+import subprocess
 
 #Dankness follows
 
-directory = os.getcwd().rsplit('/', 1)[1]
 command = ""
 
+angerLevel = 0
+
+#prompt
 laptopName = raw_input('Please enter the laptop\'s name: ')
 userName = raw_input('Please enter the user name: ')
 
-# directory
-
+#main loop
 while True: 
-	command = raw_input(laptopName +  ":" + directory + " " + userName + "$ ")
+	command = raw_input(laptopName +  ":" + os.getcwd().rsplit('/', 1)[1] + " " + userName + "$ ")
 	print command
+	root = "hello"
+	if root == "ls":
+		print "ls"
+	elif root == "cd":
+		print "cd"
+	elif root == "mkdir":
+		print "mkdir"
+	elif root == "rm":
+		print "rm"
+	elif root == "pwd":
+		print "pwd"
+	elif root == "git":
+		print "git"
+	elif root == "help":
+		print "help"
+	else:
+		try:
+			cmd = subprocess.Popen(re.split(r'\s+', command), stdout=subprocess.PIPE)
+			cmd_out = cmd.stdout.read()
+			print cmd_out
+
+		except OSError:
+			print "-bash: "+ command + " : command not found"
+	
