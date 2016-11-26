f3e91ccaa8772dd684f6380cfb811af718917010
Ivy Zhou <ivyzhou2012@hotmail.com>
2016-11-26 17:20:36 -0500

    mkdir

--- troll.py
+++ troll.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os
 import re
 import subprocess
@@ -43,15 +45,20 @@ def run_bash(command):
 		print "-bash: "+ command + " : command not found"
 		talk("do u know what ur doing")
 
+def prompt(str):
+	talk(str, False)
 
-def talk(str):
+def talk(str, newline=True):
 	time.sleep(1)
 	for c in str:
 		sys.stdout.write(colors.TALK + c)
 		sys.stdout.flush()
 		time.sleep(0.05)
 	time.sleep(1)
-	print colors.BASH
+	if newline:
+		print colors.BASH
+	else:
+		print colors.BASH,
 
 def increaseAnger(anger):
 	global angerLevel
@@ -66,8 +73,8 @@ def regulateAnger():
 def introduceSelf():
 	print colors.TALK + ASCII_COMPUTER
 	talk("hi im terminal :D")
-	talk("i'll be ur command line buddy")
-	talk("feel free to type in a few commands and i'll go and execute them for ya")
+	# talk("i'll be ur command line buddy")
+	# talk("feel free to type in a few commands and i'll go and execute them for ya")
 
 def troll_ls():
 	files =  os.listdir(os.getcwd())
@@ -80,6 +87,37 @@ def troll_rm(name):
 	print "Not like this! Please don't kill me"
 	shouldDelete = raw_input("Continue: ")
 	if shouldDelete == "y": run_bash("rm -r " + name)
+
+def troll_mkdir():
+	actions = ["orig", "print birth certificate"]
+	# strip out the name of the directory
+	mkdirOpts = command.split();
+	dirName = ""
+	for opt in mkdirOpts:
+		if opt != "mkdir" and "-" not in opt:
+			dirName = opt
+			break
+	if dirName == "":
+		talk("u have to name ur children !!!! >:{")
+		increaseAnger(1)
+		return
+	prompt("r u sure you're ready for the responsibility of a baby dir (y/n): ")
+	res = raw_input()
+	if res == "y" or res == "Y" or res == "yes" or res == "YES" or res == "Yes":
+		if res == "YES":
+			talk("wow settle down there tiger, no need to be that exited")
+		talk("a new directory was brought into the world!!! :O")
+		mum = raw_input("Please enter the father's name: ")
+		pa = raw_input("Please enter the mother's name: ")
+		print "-" * 61
+		print "| ✧･ﾟ: *✧･ﾟ:*✧･ﾟ: *✧･ﾟ:* ~ " + "BIRTH CERTIFICATE" + " ~ *:･ﾟ✧*:･ﾟ✧*:･ﾟ✧*:･ﾟ✧ |"
+		print "|" + " " * 20 + "momma: " + mum + " " * (32 - len(mum)) + "|"
+		print "|" + " " * 20 + "poppa: " + pa + " " * (32 - len(pa)) + "|"
+		print "|" + " " * 20 + "NEW LIFE: " + dirName + " " * (29 - len(dirName))  + "|"
+		print "-" * 61
+		run_bash(command)
+	else: # not ready
+		talk("ok i wasn't ready 4 that kind of commitment either")
 
 regulateAnger()
 introduceSelf()
