c678208ecb30bacb93321a3af5766a62def29e5e
utk9 <utkarshlamba.2@gmail.com>
2016-11-26 15:30:20 -0500

    Add timely regulation of terminal anger and other abstraction

--- troll.py
+++ troll.py
@@ -1,10 +1,11 @@
 import os
 import re
 import subprocess
+import threading
 
 # Dankness follows
 
-angerLevel = 0 # maxes out at 5
+angerLevel = 5 # maxes out at 5
 
 laptopName = "blah"#raw_input('Please enter the laptop\'s name: ')
 userName =  "boo"#raw_input('Please enter the user name: ')
@@ -16,6 +17,18 @@ os.system('cls' if os.name == 'nt' else 'clear')
 laptopName = raw_input('Please enter the laptop\'s name: ')
 userName = raw_input('Please enter the user name: ')
 
+def increaseAnger(anger):
+	global angerLevel
+	angerLevel = angerLevel + anger
+	if angerLevel > 5: angerLevel = 5
+
+def regulateAnger():
+  threading.Timer(60, regulateAnger).start()
+  global angerLevel
+  if angerLevel != 0: angerLevel-=1
+
+regulateAnger()
+
 #main loop
 while True:
 	command = raw_input(laptopName +  ":" + os.getcwd().rsplit('/', 1)[1] + " " + userName + "$ ")
@@ -23,7 +36,7 @@ while True:
 		root = command[0:command.index(" ")]
 	else:
 		root = command
-	print command
+	print angerLevel
 	root = "hello"
 	if root == "ls":
 		print "ls"
